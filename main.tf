48dd0dbebf705c9b87d25da20ed688f76d51e8f1
mile9299 <110489131+mile9299@users.noreply.github.com>
2026-04-15 15:44:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+cat > main.tf << 'ENDOFFILE'
 terraform {
   required_providers {
     azurerm = {
@@ -19,7 +20,6 @@ provider "azurerm" {
   features {}
 }
 
-# Data source for existing AKS cluster
 data "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name
   resource_group_name = var.resource_group_name
@@ -40,42 +40,250 @@ provider "helm" {
   }
 }
 
-# Create falcon-system namespace
 resource "kubectl_manifest" "falcon_system_namespace" {
   yaml_body = <<-YAML
-    apiVersion: v1
-    kind: Namespace
-    metadata:
-      name: falcon-system
-  YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: falcon-system
+YAML
 }
 
-# Create falcon-kac namespace
 resource "kubectl_manifest" "falcon_kac_namespace" {
   yaml_body = <<-YAML
-    apiVersion: v1
-    kind: Namespace
-    metadata:
-      name: falcon-kac
-  YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: falcon-kac
+YAML
 }
 
-# Create falcon-image-analyzer namespace
 resource "kubectl_manifest" "falcon_image_analyzer_namespace" {
   yaml_body = <<-YAML
-    apiVersion: v1
-    kind: Namespace
-    metadata:
-      name: falcon-image-analyzer
-  YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: falcon-image-analyzer
+YAML
 }
 
-# Create image pull secret for falcon-system
 resource "kubectl_manifest" "crowdstrike_pull_secret_system" {
   yaml_body = <<-YAML
-    apiVersion: v1
-    kind: Secret
-    metadata:
-      name: crowdstrike-pull-secret
-      namespace: falcon-system
-    type: kubernetes.io/dockerconfigjson
+apiVersion: v1
+kind: Secret
+metadata:
+  name: crowdstrike-pull-secret
+  namespace: falcon-system
+type: kubernetes.io/dockerconfigjson
+data:
+  .dockerconfigjson: ${base64encode(jsonencode({
+    auths = {
+      "registry.crowdstrike.com" = {
+        username = var.falcon_client_id
+        password = var.falcon_client_secret
+        auth     = base64encode("${var.falcon_client_id}:${var.falcon_client_secret}")
+      }
+    }
+  }))}
+YAML
+
+  depends_on = [kubectl_manifest.falcon_system_namespace]
+}
+
+resource "kubectl_manifest" "crowdstrike_pull_secret_kac" {
+  yaml_body = <<-YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: crowdstrike-pull-secret
+  namespace: falcon-kac
+type: kubernetes.io/dockerconfigjson
+data:
+  .dockerconfigjson: ${base64encode(jsonencode({
+    auths = {
+      "registry.crowdstrike.com" = {
+        username = var.falcon_client_id
+        password = var.falcon_client_secret
+        auth     = base64encode("${var.falcon_client_id}:${var.falcon_client_secret}")
+      }
+    }
+  }))}
+YAML
+
+  depends_on = [kubectl_manifest.falcon_kac_namespace]
+}
+
+resource "kubectl_manifest" "crowdstrike_pull_secret_iar" {
+  yaml_body = <<-YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: crowdstrike-pull-secret
+  namespace: falcon-image-analyzer
+type: kubernetes.io/dockerconfigjson
+data:
+  .dockerconfigjson: ${base64encode(jsonencode({
+    auths = {
+      "registry.crowdstrike.com" = {
+        username = var.falcon_client_id
+        password = var.falcon_client_secret
+        auth     = base64encode("${var.falcon_client_id}:${var.falcon_client_secret}")
+      }
+    }
+  }))}
+YAML
+
+  depends_on = [kubectl_manifest.falcon_image_analyzer_namespace]
+}
+
+resource "helm_release" "falcon_platform" {
+  name       = "falcon-platform"
+  repository = "https://crowdstrike.github.io/falcon-helm"
+  chart      = "falcon-platform"
+  namespace  = "falcon-system"
+  version    = var.falcon_platform_version
+
+  create_namespace = false
+  timeout          = 600
+  wait             = true
+
+  set {
+    name  = "falcon-sensor.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "falcon-sensor.falcon.cid"
+    value = var.falcon_cid
+  }
+
+  set {
+    name  = "falcon-sensor.node.backend"
+    value = "kernel"
+  }
+
+  set {
+    name  = "falcon-sensor.falcon.tags"
+    value = var.falcon_tags
+  }
+
+  set {
+    name  = "falcon-sensor.node.image.repository"
+    value = "registry.crowdstrike.com/falcon-node-sensor"
+  }
+
+  set {
+    name  = "falcon-sensor.node.image.pullSecrets"
+    value = "crowdstrike-pull-secret"
+  }
+
+  set {
+    name  = "falcon-kac.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "falcon-kac.installNamespace"
+    value = "falcon-kac"
+  }
+
+  set {
+    name  = "falcon-kac.falcon.cid"
+    value = var.falcon_cid
+  }
+
+  set {
+    name  = "falcon-kac.falcon.clientID"
+    value = var.falcon_client_id
+  }
+
+  set_sensitive {
+    name  = "falcon-kac.falcon.clientSecret"
+    value = var.falcon_client_secret
+  }
+
+  set {
+    name  = "falcon-kac.falcon.cloud"
+    value = var.falcon_cloud_region
+  }
+
+  set {
+    name  = "falcon-kac.image.repository"
+    value = "registry.crowdstrike.com/falcon-kac"
+  }
+
+  set {
+    name  = "falcon-kac.image.pullSecrets"
+    value = "crowdstrike-pull-secret"
+  }
+
+  set {
+    name  = "falcon-image-analyzer.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "falcon-image-analyzer.installNamespace"
+    value = "falcon-image-analyzer"
+  }
+
+  set {
+    name  = "falcon-image-analyzer.deployment.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "falcon-image-analyzer.crowdstrikeConfig.cid"
+    value = var.falcon_cid
+  }
+
+  set {
+    name  = "falcon-image-analyzer.crowdstrikeConfig.clientID"
+    value = var.falcon_client_id
+  }
+
+  set_sensitive {
+    name  = "falcon-image-analyzer.crowdstrikeConfig.clientSecret"
+    value = var.falcon_client_secret
+  }
+
+  set {
+    name  = "falcon-image-analyzer.crowdstrikeConfig.clusterName"
+    value = var.aks_cluster_name
+  }
+
+  set {
+    name  = "falcon-image-analyzer.crowdstrikeConfig.cloud"
+    value = var.falcon_cloud_region
+  }
+
+  set {
+    name  = "falcon-image-analyzer.image.repository"
+    value = "registry.crowdstrike.com/falcon-imageanalyzer"
+  }
+
+  set {
+    name  = "falcon-image-analyzer.image.pullSecrets"
+    value = "crowdstrike-pull-secret"
+  }
+
+  set {
+    name  = "falcon-image-analyzer.azure.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "falcon-image-analyzer.azure.subscriptionID"
+    value = var.azure_subscription_id
+  }
+
+  depends_on = [
+    kubectl_manifest.falcon_system_namespace,
+    kubectl_manifest.falcon_kac_namespace,
+    kubectl_manifest.falcon_image_analyzer_namespace,
+    kubectl_manifest.crowdstrike_pull_secret_system,
+    kubectl_manifest.crowdstrike_pull_secret_kac,
+    kubectl_manifest.crowdstrike_pull_secret_iar
+  ]
+}
+ENDOFFILE
