5a1d64709553cc5ff47ab226652f190ed0e0ab2d
mile9299 <110489131+mile9299@users.noreply.github.com>
2026-04-21 00:59:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,232 +45,55 @@ provider "helm" {
   }
 }
 
-resource "kubectl_manifest" "falcon_system_namespace" {
-  yaml_body = <<-YAML
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: falcon-system
-YAML
+# Use official CrowdStrike Falcon Sensor module
+module "falcon_sensor" {
+  source = "github.com/CrowdStrike/terraform-kubectl-falcon//modules/falcon_sensor"
+
+  client_id     = var.falcon_client_id
+  client_secret = var.falcon_client_secret
+  cid           = var.falcon_cid
+  cloud         = var.falcon_cloud_region
+
+  sensor_image_tag        = var.falcon_sensor_version
+  sensor_image_repository = "registry.crowdstrike.com/falcon-sensor/release/falcon-sensor"
+  sensor_image_pull_token = var.falcon_registry_pull_token
+
+  node_backend = "bpf"
+  tags         = var.falcon_tags
 }
 
-resource "kubectl_manifest" "falcon_kac_namespace" {
-  yaml_body = <<-YAML
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: falcon-kac
-YAML
+# Use official CrowdStrike Falcon KAC module
+module "falcon_kac" {
+  source = "github.com/CrowdStrike/terraform-kubectl-falcon//modules/falcon_kac"
+
+  client_id     = var.falcon_client_id
+  client_secret = var.falcon_client_secret
+  cid           = var.falcon_cid
+  cloud         = var.falcon_cloud_region
+
+  kac_image_tag        = var.falcon_kac_version
+  kac_image_repository = "registry.crowdstrike.com/falcon-kac/release/falcon-kac"
+  kac_image_pull_token = var.falcon_kac_pull_token
+
+  cluster_name = local.cluster_resource_id_falcon
+
+  depends_on = [module.falcon_sensor]
 }
 
-resource "kubectl_manifest" "falcon_image_analyzer_namespace" {
-  yaml_body = <<-YAML
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: falcon-image-analyzer
-YAML
-}
+# Use official CrowdStrike Falcon IAR module
+module "falcon_iar" {
+  source = "github.com/CrowdStrike/terraform-kubectl-falcon//modules/falcon_iar"
 
-resource "kubectl_manifest" "crowdstrike_pull_secret_system" {
-  yaml_body = <<-YAML
-apiVersion: v1
-kind: Secret
-metadata:
-  name: crowdstrike-pull-secret
-  namespace: falcon-system
-type: kubernetes.io/dockerconfigjson
-data:
-  .dockerconfigjson: ${var.falcon_registry_pull_token}
-YAML
+  client_id     = var.falcon_client_id
+  client_secret = var.falcon_client_secret
+  cid           = var.falcon_cid
+  cloud         = var.falcon_cloud_region
 
-  depends_on = [kubectl_manifest.falcon_system_namespace]
-}
+  iar_image_tag        = var.falcon_iar_version
+  iar_image_repository = "registry.crowdstrike.com/falcon-imageanalyzer/${var.falcon_cloud_region}/release/falcon-imageanalyzer"
+  iar_image_pull_token = var.falcon_iar_pull_token
 
-# -----------------------------------------------
-# Falcon Sensor - DaemonSet in falcon-system
-# -----------------------------------------------
-resource "helm_release" "falcon_sensor" {
-  name             = "falcon-sensor"
-  repository       = "https://crowdstrike.github.io/falcon-helm"
-  chart            = "falcon-sensor"
-  namespace        = "falcon-system"
-  create_namespace = false
-  timeout          = 600
-  wait             = true
+  cluster_name = local.cluster_resource_id_falcon
 
-  set {
-    name  = "falcon.cid"
-    value = var.falcon_cid
-  }
-
-  set {
-    name  = "node.backend"
-    value = "bpf"
-  }
-
-  set {
-    name  = "falcon.tags"
-    value = var.falcon_tags
-  }
-
-  set {
-    name  = "node.image.repository"
-    value = "registry.crowdstrike.com/falcon-sensor/release/falcon-sensor"
-  }
-
-  set {
-    name  = "node.image.tag"
-    value = var.falcon_sensor_version
-  }
-
-  set {
-    name  = "node.image.pullSecrets"
-    value = "crowdstrike-pull-secret"
-  }
-
-  depends_on = [
-    kubectl_manifest.falcon_system_namespace,
-    kubectl_manifest.crowdstrike_pull_secret_system
-  ]
-}
-
-# -----------------------------------------------
-# Falcon KAC - in falcon-kac namespace
-# Removed falcon.cloudProvider - not valid falconctl option
-# -----------------------------------------------
-resource "helm_release" "falcon_kac" {
-  name             = "falcon-kac"
-  repository       = "https://crowdstrike.github.io/falcon-helm"
-  chart            = "falcon-kac"
-  namespace        = "falcon-kac"
-  create_namespace = false
-  timeout          = 600
-  wait             = true
-
-  set {
-    name  = "falcon.cid"
-    value = var.falcon_cid
-  }
-
-  set {
-    name  = "falcon.cloud"
-    value = var.falcon_cloud_region
-  }
-
-  set {
-    name  = "image.repository"
-    value = "registry.crowdstrike.com/falcon-kac/release/falcon-kac"
-  }
-
-  set {
-    name  = "image.tag"
-    value = var.falcon_kac_version
-  }
-
-  set_sensitive {
-    name  = "image.registryConfigJSON"
-    value = var.falcon_kac_pull_token
-  }
-
-  set {
-    name  = "clusterName"
-    value = local.cluster_resource_id_falcon
-  }
-
-  set {
-    name  = "cloudProvider"
-    value = "azure"
-  }
-
-  set {
-    name  = "azure.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "azure.subscriptionID"
-    value = var.azure_subscription_id
-  }
-
-  set {
-    name  = "azure.location"
-    value = data.azurerm_kubernetes_cluster.aks.location
-  }
-
-  depends_on = [
-    kubectl_manifest.falcon_kac_namespace
-  ]
-}
-
-# -----------------------------------------------
-# Falcon IAR - in falcon-image-analyzer namespace
-# -----------------------------------------------
-resource "helm_release" "falcon_iar" {
-  name             = "falcon-imageanalyzer"
-  repository       = "https://crowdstrike.github.io/falcon-helm"
-  chart            = "falcon-image-analyzer"
-  namespace        = "falcon-image-analyzer"
-  create_namespace = false
-  timeout          = 600
-  wait             = true
-
-  set {
-    name  = "crowdstrikeConfig.cid"
-    value = var.falcon_cid
-  }
-
-  set {
-    name  = "crowdstrikeConfig.clientID"
-    value = var.falcon_client_id
-  }
-
-  set_sensitive {
-    name  = "crowdstrikeConfig.clientSecret"
-    value = var.falcon_client_secret
-  }
-
-  set {
-    name  = "crowdstrikeConfig.clusterName"
-    value = local.cluster_resource_id_falcon
-  }
-
-  set {
-    name  = "crowdstrikeConfig.cloud"
-    value = var.falcon_cloud_region
-  }
-
-  set {
-    name  = "deployment.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "image.repository"
-    value = "registry.crowdstrike.com/falcon-imageanalyzer/${var.falcon_cloud_region}/release/falcon-imageanalyzer"
-  }
-
-  set {
-    name  = "image.tag"
-    value = var.falcon_iar_version
-  }
-
-  set_sensitive {
-    name  = "image.registryConfigJSON"
-    value = var.falcon_iar_pull_token
-  }
-
-  set {
-    name  = "azure.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "azure.subscriptionID"
-    value = var.azure_subscription_id
-  }
-
-  depends_on = [
-    kubectl_manifest.falcon_image_analyzer_namespace
-  ]
+  depends_on = [module.falcon_sensor]
 }
